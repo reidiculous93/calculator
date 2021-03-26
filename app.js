afecea012b6abace34c9de3d2112a114575aa4b1
Reid Vrana <reidvrana93@gmail.com>
2021-03-26 13:40:58 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+body {
+    background-color: #edc077;
+  }
+  
+  #flex-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 400px;
+    height: 500px;
+    margin: auto;
+    border: 2px solid black;
+    border-radius: 15px;
+    background-color: #757574;
+  }
+  
+  #display {
+    width: 100%;
+    height: 6rem;
+    margin: 20px;
+    border-radius: 10px;
+    border: 2px solid black;
+    background-color: #d9d9d9;
+  }
+  
+  #clear {
+    width: 75%;
+    height: 4rem;
+    border-radius: 10px;
+    border: 1px solid black;
+    margin: 0 auto;
+    background-color: #fc5863;
+  }
+  
+  .number-btn {
+    width: 20%;
+    height: 4rem;
+    border-radius: 10px;
+    border: 1px solid black;
+    margin: 0 auto;
+    background-color: #d9d9d9;
+  }
